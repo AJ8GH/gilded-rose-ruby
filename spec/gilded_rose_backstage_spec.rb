bc6b29007cc19c0c671b8e733bac047bdb1b1f2c
Adam Jonas <adamjonas25@gmail.com>
2021-04-08 17:06:10 +0100

    Add quality to zero test for backstage pass

--- spec/gilded_rose_backstage_spec.rb
+++ spec/gilded_rose_backstage_spec.rb
@@ -1,12 +1,10 @@
 describe GildedRose do
   describe '#update_quality' do
     name = 'Backstage passes to a TAFKAL80ETC concert'
-    let(:items0) { [Item.new(name, 11, 1)] }
-    let(:items1) { [Item.new(name, 10, 1)] }
-    let(:items2) { [Item.new(name, 6, 1)] }
-    let(:items3) { [Item.new(name, 5, 1)] }
 
     describe 'backstage pass' do
+      let(:items0) { [Item.new(name, 11, 1)] }
+
       it 'reduces sell in value' do
         expect {
           GildedRose.new(items0).update_quality
@@ -14,21 +12,24 @@ describe GildedRose do
       end
 
       context 'when sell in value > 10' do
-        it 'increase quality by 1' do
+        it 'increases quality by 1' do
           expect {
             GildedRose.new(items0).update_quality
           }.to change { items0[0].quality }.by(1)
         end
       end
 
-      context 'when sell in value < 10 && > 5' do
-        it 'increase quality by 2 at sell in == 10' do
+      context 'when sell in value is between 10 and 5' do
+        let(:items1) { [Item.new(name, 10, 1)] }
+        let(:items2) { [Item.new(name, 6, 1)] }
+
+        it 'increases quality by 2 when sell in is 10' do
           expect {
             GildedRose.new(items1).update_quality
           }.to change { items1[0].quality }.by(2)
         end
 
-        it 'increase quality by 2 at sell in == 6' do
+        it 'increases quality by 2 when sell in is 6' do
           expect {
             GildedRose.new(items2).update_quality
           }.to change { items2[0].quality }.by(2)
@@ -36,10 +37,28 @@ describe GildedRose do
       end
 
       context 'when sell in value <= 5' do
-        it 'increase quality by 3' do
+        let(:items3) { [Item.new(name, 5, 1)] }
+        let(:items4) { [Item.new(name, 1, 1)] }
+
+        it 'increases quality by 3 when sell in is 5' do
           expect {
             GildedRose.new(items3).update_quality
           }.to change { items3[0].quality }.by(3)
+        end
+
+        it 'increases quality by 3 when sell in is 1' do
+          expect {
+            GildedRose.new(items4).update_quality
+          }.to change { items4[0].quality }.by(3)
+        end
+      end
+
+      context 'when sell in value is zero' do
+        let(:items5) { [Item.new(name, 0, 50)] }
+
+        it 'reduces quality to zero' do
+          GildedRose.new(items5).update_quality
+          expect(items5[0].quality).to be_zero
         end
       end
     end
