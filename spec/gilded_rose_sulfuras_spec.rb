e1b2a952a27b7f08c181e9108ffcff0e2a8a8cda
Adam Jonas <adamjonas25@gmail.com>
2021-04-08 16:05:50 +0100

    Add ruby to gemfile lock platform, Add test for sulfuras sell in

--- spec/gilded_rose_sulfuras_spec.rb
+++ spec/gilded_rose_sulfuras_spec.rb
@@ -1,15 +1,14 @@
 describe GildedRose do
-  xdescribe '#update_quality' do
-    # let(:items0) { [Item.new('Aged Brie', nil, 80)] }
-    # let(:items1) { [Item.new('Aged Brie', 0, 0)] }
+  describe '#update_quality' do
+    let(:items) { [Item.new('Sulfuras, Hand of Ragnaros', 0, 80)] }
 
-    # describe 'Sulfuras' do
-    #   it 'reduces sell in value' do
-    #     expect {
-    #       GildedRose.new(items0).update_quality
-    #     }.to change { items0[0].sell_in }.by(-1)
-    #   end
-    # end
+    describe 'Sulfuras' do
+      it 'does not reduce sell in value' do
+        expect {
+          GildedRose.new(items).update_quality
+        }.to change { items[0].sell_in }.by(0)
+      end
+    end
 
     #   context 'when sell_in > 0' do
     #     it 'increases quality value by 1' do
