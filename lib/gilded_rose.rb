30f1fc80e8a52e0490f773aaf9b24e273b641ecf
Adam Jonas <adamjonas25@gmail.com>
2021-04-08 18:34:26 +0100

    Refactor using inline if statements and shorthand incrementation where possible

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -1,5 +1,4 @@
 class GildedRose
-
   def initialize(items)
     @items = items
   end
@@ -8,49 +7,33 @@ class GildedRose
     @items.each do |item|
       if item.name != 'Aged Brie' and item.name != 'Backstage passes to a TAFKAL80ETC concert'
         if item.quality > 0
-          if item.name != 'Sulfuras, Hand of Ragnaros'
-            item.quality = item.quality - 1
-          end
+          item.quality -= 1 if item.name != 'Sulfuras, Hand of Ragnaros'
         end
-
       else
-        if item.quality < 50
-          item.quality = item.quality + 1
+        item.quality += 1 if item.quality < 50
           if item.name == 'Backstage passes to a TAFKAL80ETC concert'
             if item.sell_in < 11
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-
+              item.quality += 1 if item.quality < 50
             if item.sell_in < 6
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
+              item.quality += 1 if item.quality < 50
             end
           end
         end
       end
 
-      if item.name != 'Sulfuras, Hand of Ragnaros'
-        item.sell_in = item.sell_in - 1
-      end
+      item.sell_in -= 1 if item.name != 'Sulfuras, Hand of Ragnaros'
 
       if item.sell_in < 0
         if item.name != 'Aged Brie'
           if item.name != 'Backstage passes to a TAFKAL80ETC concert'
             if item.quality > 0
-              if item.name != 'Sulfuras, Hand of Ragnaros'
-                item.quality = item.quality - 1
-              end
+              item.quality -= 1 if item.name != 'Sulfuras, Hand of Ragnaros'
             end
-
           else
-            item.quality = item.quality - item.quality
+            item.quality = 0
           end
-
         elsif item.quality < 50
-          item.quality = item.quality + 1
+          item.quality += 1
         end
       end
     end
