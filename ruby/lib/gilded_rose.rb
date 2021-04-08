9d4fc94850eab08d40eb654e85adf3575887ca6f
Adam Jonas <adamjonas25@gmail.com>
2021-04-08 13:14:33 +0100

    Fix failing unit test

--- ruby/lib/gilded_rose.rb
+++ ruby/lib/gilded_rose.rb
@@ -12,6 +12,7 @@ class GildedRose
             item.quality = item.quality - 1
           end
         end
+
       else
         if item.quality < 50
           item.quality = item.quality + 1
@@ -21,6 +22,7 @@ class GildedRose
                 item.quality = item.quality + 1
               end
             end
+
             if item.sell_in < 6
               if item.quality < 50
                 item.quality = item.quality + 1
@@ -29,9 +31,11 @@ class GildedRose
           end
         end
       end
+
       if item.name != "Sulfuras, Hand of Ragnaros"
         item.sell_in = item.sell_in - 1
       end
+
       if item.sell_in < 0
         if item.name != "Aged Brie"
           if item.name != "Backstage passes to a TAFKAL80ETC concert"
@@ -40,29 +44,15 @@ class GildedRose
                 item.quality = item.quality - 1
               end
             end
+
           else
             item.quality = item.quality - item.quality
           end
-        else
-          if item.quality < 50
-            item.quality = item.quality + 1
-          end
+
+        elsif item.quality < 50
+          item.quality = item.quality + 1
         end
       end
     end
-  end
-end
-
-class Item
-  attr_accessor :name, :sell_in, :quality
-
-  def initialize(name, sell_in, quality)
-    @name = name
-    @sell_in = sell_in
-    @quality = quality
-  end
-
-  def to_s()
-    "#{@name}, #{@sell_in}, #{@quality}"
   end
 end
