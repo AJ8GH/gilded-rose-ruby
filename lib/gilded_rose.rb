e719fb4b5acf622cdb3107c4498bec3a4fb30e85
Adam Jonas <adamjonas25@gmail.com>
2021-04-08 19:29:12 +0100

    Add predicate methods to check items

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -6,6 +6,9 @@ class GildedRose
   BACKSTAGE_DOUBLE_QUALITY = 10
   BACKSTAGE_TRIPLE_QUALITY = 5
   SELL_BY_DATE = 0
+  SULFURAS = 'Sulfuras, Hand of Ragnaros'
+  AGED_BRIE = 'Aged Brie'
+  BACKSTAGE_PASSES = 'Backstage passes to a TAFKAL80ETC concert'
 
   def initialize(items)
     @items = items
@@ -13,28 +16,42 @@ class GildedRose
 
   def update_quality()
     @items.each do |item|
-      unless item.name == 'Aged Brie' || item.name == 'Backstage passes to a TAFKAL80ETC concert'
-        item.quality -= NORMAL_QUALITY_CHANGE if item.name != 'Sulfuras, Hand of Ragnaros' && item.quality > MINIMUM_QUALITY
+      unless brie?(item) || passes?(item)
+        item.quality -= NORMAL_QUALITY_CHANGE if !sulfuras?(item) && item.quality > MINIMUM_QUALITY
       else
         item.quality += NORMAL_QUALITY_CHANGE if item.quality < MAXIMUM_QUALITY
       end
 
-      if item.name == 'Backstage passes to a TAFKAL80ETC concert' && item.sell_in <= BACKSTAGE_DOUBLE_QUALITY
+      if passes?(item) && item.sell_in <= BACKSTAGE_DOUBLE_QUALITY
         item.quality += NORMAL_QUALITY_CHANGE if item.quality < MAXIMUM_QUALITY
         item.quality += NORMAL_QUALITY_CHANGE if item.sell_in <= BACKSTAGE_TRIPLE_QUALITY && item.quality < MAXIMUM_QUALITY
       end
 
-      item.sell_in -= SELL_IN_REDUCTION if item.name != 'Sulfuras, Hand of Ragnaros'
+      item.sell_in -= SELL_IN_REDUCTION if !sulfuras?(item)
 
       if item.sell_in < SELL_BY_DATE
-        if item.name == 'Aged Brie'
+        if brie?(item)
           item.quality += NORMAL_QUALITY_CHANGE unless item.quality >= MAXIMUM_QUALITY
-        elsif item.name != 'Backstage passes to a TAFKAL80ETC concert'
-          item.quality -= NORMAL_QUALITY_CHANGE if item.quality > MINIMUM_QUALITY && item.name != 'Sulfuras, Hand of Ragnaros'
+        elsif !passes?(item)
+          item.quality -= NORMAL_QUALITY_CHANGE if item.quality > MINIMUM_QUALITY && !sulfuras?(item)
         else
           item.quality = MINIMUM_QUALITY
         end
       end
     end
+  end
+
+  private
+
+  def sulfuras?(item)
+    item.name == SULFURAS
+  end
+
+  def brie?(item)
+    item.name == AGED_BRIE
+  end
+
+  def passes?(item)
+    item.name == BACKSTAGE_PASSES
   end
 end
