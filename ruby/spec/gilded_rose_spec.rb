8c5c2279215bbac1d179c4486182d2b4ab2d7af2
Adam Jonas <adamjonas25@gmail.com>
2021-04-08 15:16:26 +0100

    Add all tests for regular items

--- ruby/spec/gilded_rose_spec.rb
+++ ruby/spec/gilded_rose_spec.rb
@@ -1,11 +1,23 @@
 describe GildedRose do
   describe '#update_quality' do
-    items0 = [Item.new('foo', 0, 0)]
-    items1 = [Item.new('foo', 1, 1)]
-    items2 = [Item.new('foo', -1, 2)]
+    let(:items0) { [Item.new('foo', 0, 0)] }
+    let(:items1) { [Item.new('foo', 1, 1)] }
+    let(:items2) { [Item.new('foo', -1, 2)] }
 
     describe 'regular items' do
+      it 'reduces sell in value' do
+        expect {
+          GildedRose.new(items1).update_quality
+        }.to change { items1[0].sell_in }.by(-1)
+      end
+
       context 'when sell in value is positive' do
+        it 'reduces item quality by 1' do
+          expect {
+            GildedRose.new(items1).update_quality
+          }.to change { items1[0].quality }.by(-1)
+        end
+
         it 'does not change the name' do
           GildedRose.new(items0).update_quality
           expect(items0[0].name).to eq 'foo'
@@ -14,12 +26,6 @@ describe GildedRose do
         it 'does not reduce item quality past 0' do
           GildedRose.new(items0).update_quality
           expect(items0[0].quality).to be_zero
-        end
-
-        it 'reduces item quality when yet not at 0' do
-          expect {
-            GildedRose.new(items1).update_quality
-          }.to change { items1[0].quality }.by(-1)
         end
       end
 
