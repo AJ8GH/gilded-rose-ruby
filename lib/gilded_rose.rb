7d012d4b2f6b0c49a346755a8960e8175c159c82
Adam Jonas <adamjonas25@gmail.com>
2021-04-08 18:48:02 +0100

    Replace magic numbers with constants

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -1,4 +1,11 @@
 class GildedRose
+  MINIMUM_QUALITY = 0
+  MAXIMUM_QUALITY = 50
+  NORMAL_QUALITY_CHANGE = 1
+  BACKSTAGE_DOUBLE_QUALITY = 10
+  BACKSTAGE_TRIPLE_QUALITY = 5
+  SELL_BY_DATE = 0
+
   def initialize(items)
     @items = items
   end
@@ -6,34 +13,34 @@ class GildedRose
   def update_quality()
     @items.each do |item|
       if item.name != 'Aged Brie' and item.name != 'Backstage passes to a TAFKAL80ETC concert'
-        if item.quality > 0
-          item.quality -= 1 if item.name != 'Sulfuras, Hand of Ragnaros'
+        if item.quality > MINIMUM_QUALITY
+          item.quality -= NORMAL_QUALITY_CHANGE if item.name != 'Sulfuras, Hand of Ragnaros'
         end
       else
-        item.quality += 1 if item.quality < 50
+        item.quality += NORMAL_QUALITY_CHANGE if item.quality < MAXIMUM_QUALITY
           if item.name == 'Backstage passes to a TAFKAL80ETC concert'
-            if item.sell_in < 11
-              item.quality += 1 if item.quality < 50
-            if item.sell_in < 6
-              item.quality += 1 if item.quality < 50
+            if item.sell_in <= BACKSTAGE_DOUBLE_QUALITY
+              item.quality += NORMAL_QUALITY_CHANGE if item.quality < MAXIMUM_QUALITY
+            if item.sell_in <= BACKSTAGE_TRIPLE_QUALITY
+              item.quality += NORMAL_QUALITY_CHANGE if item.quality < MAXIMUM_QUALITY
             end
           end
         end
       end
 
-      item.sell_in -= 1 if item.name != 'Sulfuras, Hand of Ragnaros'
+      item.sell_in -= NORMAL_QUALITY_CHANGE if item.name != 'Sulfuras, Hand of Ragnaros'
 
-      if item.sell_in < 0
+      if item.sell_in < SELL_BY_DATE
         if item.name != 'Aged Brie'
           if item.name != 'Backstage passes to a TAFKAL80ETC concert'
-            if item.quality > 0
-              item.quality -= 1 if item.name != 'Sulfuras, Hand of Ragnaros'
+            if item.quality > MINIMUM_QUALITY
+              item.quality -= NORMAL_QUALITY_CHANGE if item.name != 'Sulfuras, Hand of Ragnaros'
             end
           else
-            item.quality = 0
+            item.quality = MINIMUM_QUALITY
           end
-        elsif item.quality < 50
-          item.quality += 1
+        elsif item.quality < MAXIMUM_QUALITY
+          item.quality += NORMAL_QUALITY_CHANGE
         end
       end
     end
