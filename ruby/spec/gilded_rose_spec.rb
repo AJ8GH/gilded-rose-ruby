c5e7766ef1e969bd43ef079f6f1c75f3737f684d
Adam Jonas <adamjonas25@gmail.com>
2021-04-08 15:01:20 +0100

    add tests for reducing item quality

--- ruby/spec/gilded_rose_spec.rb
+++ ruby/spec/gilded_rose_spec.rb
@@ -1,19 +1,22 @@
 describe GildedRose do
   describe '#update_quality' do
-    it 'does not change the name' do
-      items = [Item.new('foo', 0, 0)]
-      GildedRose.new(items).update_quality()
+    items = [Item.new('foo', 0, 0)]
+    items2 = [Item.new('foo', 1, 1)]
 
+    it 'does not change the name' do
+      GildedRose.new(items).update_quality()
       expect(items[0].name).to eq 'foo'
     end
-  end
 
-  describe '#update_quality' do
-    it 'does not change the name' do
-      items = [Item.new('foo', 0, 0)]
+    it 'does not reduce item quality past 0' do
       GildedRose.new(items).update_quality()
+      expect(items[0].quality).to be_zero
+    end
 
-      expect(items[0].name).to eq 'foo'
+    it 'reduces item quality when yet not at 0' do
+      expect(items2[0].quality).to be(1)
+      GildedRose.new(items2).update_quality()
+      expect(items2[0].quality).to be_zero
     end
   end
 end
