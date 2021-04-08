4031c22d3dced2959be8e6c79f1527b92f579cba
Adam Jonas <adamjonas25@gmail.com>
2021-04-08 16:50:26 +0100

    Add test for backstage pass when sell in value == 5

--- spec/gilded_rose_backstage_spec.rb
+++ spec/gilded_rose_backstage_spec.rb
@@ -4,6 +4,7 @@ describe GildedRose do
     let(:items0) { [Item.new(name, 11, 1)] }
     let(:items1) { [Item.new(name, 10, 1)] }
     let(:items2) { [Item.new(name, 6, 1)] }
+    let(:items3) { [Item.new(name, 5, 1)] }
 
     describe 'backstage pass' do
       it 'reduces sell in value' do
@@ -31,6 +32,14 @@ describe GildedRose do
           expect {
             GildedRose.new(items2).update_quality
           }.to change { items2[0].quality }.by(2)
+        end
+      end
+
+      context 'when sell in value <= 5' do
+        it 'increase quality by 3' do
+          expect {
+            GildedRose.new(items3).update_quality
+          }.to change { items3[0].quality }.by(3)
         end
       end
     end
