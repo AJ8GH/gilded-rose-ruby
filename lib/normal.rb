e19d85df336f32aaa6add6061e5ab0eacde4b801
Adam Jonas <adamjonas25@gmail.com>
2021-04-11 20:17:53 +0100

    Add minimum quality limit to normal item

--- lib/normal.rb
+++ lib/normal.rb
@@ -1,6 +1,7 @@
 class Normal < Item
   def update_quality
+    sell_in > 0 ? self.quality -= 1 : self.quality -= 2
+    self.quality = 0 if quality < 0
     self.sell_in -= 1
-    self.quality -= 1
   end
 end
--- spec/normal_spec.rb
+++ spec/normal_spec.rb
@@ -6,9 +6,23 @@ describe Normal do
       expect { normal.update_quality }.to change { normal.sell_in }.by(-1)
     end
 
-    context 'when sell in > 0'
-    it 'reduces quality by 1' do
-      expect { normal.update_quality }.to change { normal.quality }.by(-1)
+    context 'when sell in > 0' do
+      it 'reduces quality by 1' do
+        expect { normal.update_quality }.to change { normal.quality }.by(-1)
+      end
+    end
+
+    context 'when sell in <= 0' do
+      subject(:normal) { described_class.new('normal', 0, 2) }
+
+      it 'reduces quality by 2' do
+        expect { normal.update_quality }.to change { normal.quality }.by(-2)
+      end
+    end
+
+    it 'never reduces quality below 0' do
+      100.times { normal.update_quality }
+      expect(normal.quality).to be_zero
     end
   end
 end
