b112f0f038938f584d938993d1d3760a14a7701a
Adam Jonas <adamjonas25@gmail.com>
2021-04-11 19:03:58 +0100

    Implement max quality in aged brie sub class

--- lib/aged_brie.rb
+++ lib/aged_brie.rb
@@ -1,4 +1,6 @@
 class AgedBrie < Item
+  MAXIMUM_QUALITY = 50
+
   def initialize(name = 'Aged Brie', sell_in, quality)
     @name = name
     super
@@ -6,6 +8,9 @@ class AgedBrie < Item
 
   def update_quality
     self.sell_in -= 1
+    self.quality = MAXIMUM_QUALITY if quality > MAXIMUM_QUALITY
+    return if quality == MAXIMUM_QUALITY
+
     sell_in > 0 ? self.quality += 1 : self.quality += 2
   end
 end
--- spec/aged_brie_spec.rb
+++ spec/aged_brie_spec.rb
@@ -1,6 +1,5 @@
 describe AgedBrie do
   subject(:aged_brie) { described_class.new(5, 5) }
-  subject(:double_aged_brie) { described_class.new(0, 5) }
 
   describe '#update_quality' do
     it 'reduces the sell in by 1' do
@@ -16,11 +15,18 @@ describe AgedBrie do
     end
 
     context 'when sell in <= 0' do
+      subject(:double_aged_brie) { described_class.new(0, 5) }
+
       it 'increases quality value by 2' do
         expect {
           double_aged_brie.update_quality
         }.to change { double_aged_brie.quality }.by(2)
       end
+    end
+
+    it 'does not increase quality past 50' do
+      100.times { aged_brie.update_quality }
+      expect(aged_brie.quality).to be(50)
     end
   end
 end
