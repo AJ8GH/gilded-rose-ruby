082d3bde28ec923bed1848b965d3e3613f2819b1
Adam Jonas <adamjonas25@gmail.com>
2021-04-11 19:41:51 +0100

    implement maximum quality in backstage passes

--- lib/backstage_passes.rb
+++ lib/backstage_passes.rb
@@ -1,13 +1,18 @@
 class BackstagePasses < Item
+  MAXIMUM_QUALITY = 50
+
   def initialize(name = 'Backstage passes to a TAFKAL80ETC concert', sell_in, quality)
     super
   end
 
   def update_quality
+    return if quality == MAXIMUM_QUALITY
+
     sell_in > 10 ? self.quality += 1 :
     sell_in > 5 ? self.quality += 2 :
     sell_in > 0 ? self.quality += 3 : self.quality = 0
 
+    self.quality = MAXIMUM_QUALITY if quality > MAXIMUM_QUALITY
     self.sell_in -= 1
   end
 end
--- spec/backstage_passes_spec.rb
+++ spec/backstage_passes_spec.rb
@@ -44,5 +44,15 @@ describe BackstagePasses do
         expect(backstage_passes.quality).to be_zero
       end
     end
+
+    it 'stops increasing when quality hits 50' do
+      backstage_passes =  described_class.new(3, 49)
+
+      backstage_passes.update_quality
+      expect(backstage_passes.quality).to be(50)
+
+      backstage_passes.update_quality
+      expect(backstage_passes.quality).to be(50)
+    end
   end
 end
