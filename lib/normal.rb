5594a9ed0525fc543188eb86cba99d01d8e66d63
Adam Jonas <adamjonas25@gmail.com>
2021-04-11 20:13:36 +0100

    implement reduce quality in normal items

--- lib/normal.rb
+++ lib/normal.rb
@@ -1,5 +1,6 @@
 class Normal < Item
   def update_quality
     self.sell_in -= 1
+    self.quality -= 1
   end
 end
--- spec/normal_spec.rb
+++ spec/normal_spec.rb
@@ -3,9 +3,12 @@ describe Normal do
 
   describe '#update_quality' do
     it 'reduces sell in by 1' do
-      expect {
-        normal.update_quality
-      }.to change { normal.sell_in }.by(-1)
+      expect { normal.update_quality }.to change { normal.sell_in }.by(-1)
+    end
+
+    context 'when sell in > 0'
+    it 'reduces quality by 1' do
+      expect { normal.update_quality }.to change { normal.quality }.by(-1)
     end
   end
 end
